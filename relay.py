b7e79053ef124a949d69f60bd8ac8361386394d5
Rigel <112126663+RigelLM@users.noreply.github.com>
2024-11-11 18:21:15 -0500

    - Fix relay bug that turns on and off regardless of the status of display

--- relay.py
+++ relay.py
@@ -1,23 +1,41 @@
 import RPi.GPIO as gpio
-from time import sleep
 import gpiozero
-import os
 
+import Camera
+import VideoProcess
+
+from time import sleep
+
+# GPIO pin setup
+ON = True
+OFF = not ON
 
 RELAY_PIN = 24
-
 gpio.setmode(gpio.BCM)
 
 relay = gpiozero.OutputDevice(RELAY_PIN, active_high=True, initial_value=False)
 
-def set_relay(status):
-    if status:
-        print("Setting relay: ON")
-        relay.on()
-    else:
-        print("Setting relay: OFF")
-        relay.off()
+# Mimic pressing sensor button for i seconds
+def press_sensor_btn(i):
+    relay.on()
+    sleep(i)
+    relay.off()
 
-# if not os.path.exists("monitor_on"):
-#     with open("monitor_on", 'w') as file:
-#         pass
+# Set the sensor display
+def set_sensor(on):
+    sensor_image = Camera.get_image_2_array()
+    currently_on = VideoProcess.check_display(sensor_image)
+    if currently_on:
+        if on:
+            print("Sensor already on")
+        else:
+            press_sensor_btn(0.5)
+            print("Sensor off")
+    else:
+        if on:
+            press_sensor_btn(0,5)
+            print("Sensor on")
+            # Wait til display is ready
+            sleep(10)
+        else:
+            print("Sensor already off")
