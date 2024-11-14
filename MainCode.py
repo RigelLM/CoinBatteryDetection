7648fdec43492017b6beab1458a0207e67871967
RigelLM <limian1105@gmail.com>
2024-11-13 20:18:12 -0500

    - Fix bugs
    - Fix bug that video files cannot be written when Temp does not exist
    - Fix bug that callibration won't be executed if sensor stops during scanning but main code stays on

--- MainCode.py
+++ MainCode.py
@@ -10,6 +10,7 @@ import Relay
 from time import sleep
 import threading
 import os
+from datetime import datetime
 
 # GPIO pin setup
 button= Button(26)
@@ -19,21 +20,52 @@ led_white = LED(27)
 led_blue = LED(18)
 led_yellow = LED(17)
 
+def write_reference(file_name, result):
+    if not os.path.exists('Temp/Reference.txt'):
+        with open('Temp/Reference.txt', "w") as file:
+            file.write(datetime.now().strftime("%Y%m%d_%H%M%S"))
+
+    with open("Temp/Reference.txt", "a") as file:
+        # Write file_name and result
+        file.write(f"{file_name} {result}\n")
+
 def main():
+
+    print("Setting up storage folders")
+    if not os.path.exists('Temp'):
+        os.makedirs('Temp')
+        os.makedirs('Temp/Original')
+        os.makedirs('Temp/Processed')
+    if os.path.exists('Temp'):
+        if not os.path.exists('Temp/Original'):
+            os.makedirs('Temp/Original')
+        if not os.path.exists('Temp/Processed'):
+            os.makedirs('Temp/Processed')
+
+    ori_filename = "./Temp/Original/" + datetime.now().strftime("%Y%m%d_%H%M%S") + ".mp4"
+    pro_filename = "./Temp/Processed/" + datetime.now().strftime("%Y%m%d_%H%M%S") + "_p.mp4"
+
     print("Start scanning ...")
-    Scan.scan("./Temp/v.mp4")
+    Scan.scan(ori_filename)
     sleep(2)
 
     print("Preparing for detection ...")
-    VideoProcess.crop_video("./Temp/v.mp4", "./Temp/v_p.mp4")
+    VideoProcess.crop_video(ori_filename, pro_filename)
     sleep(2)
 
     print("Processing scanning result ...")
     return ClusterDetection.detect("./Temp/v_p.mp4")
+    result =  ClusterDetection.detect(pro_filename)
+
+    write_reference(ori_filename, result)
+
+    return result
 
 def show(result):
-    if result == "Metal" or result == "Battery":
+    if result == "Metal":
         led_yellow.on()
+    elif result == "Battery":
+        led_blue.on()
     else:
         led_white.on()
 
@@ -73,6 +105,11 @@ if __name__ == "__main__":
             if button.is_pressed:
                 print("Button pressed")
 
+                # Do callibration before every scan
+                callibration_pass = Scan.callibration()
+                if not callibration_pass:
+                    break
+
                 if not in_progress:
                     in_progress = True
 
@@ -90,9 +127,6 @@ if __name__ == "__main__":
                     in_progress = False
                 else:
                     print("Already in progress")
-                # Do callibration after every scan
-                callibration_pass = Scan.callibration()
-        # If callibration fails, pause the whole system and wait for reboot
         while True:
             # Reboot the whole system if button is pressed
             if button.is_pressed:
